f48bc548f79016a3c64d59084617f219f27293d7
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2022-01-30 23:24:40 +0530

    change default theme files

--- app/src/main/java/dev/saurabhmishra/redditclone/theme/Theme.kt
+++ app/src/main/java/dev/saurabhmishra/redditclone/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = RedditCloneColors.purple200,
+    primaryVariant = RedditCloneColors.purple700,
+    secondary = RedditCloneColors.teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = RedditCloneColors.purple500,
+    primaryVariant = RedditCloneColors.purple700,
+    secondary = RedditCloneColors.teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -37,8 +37,8 @@ fun RedditCloneTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compo
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+        typography = RedditCloneTypography.typography,
+        shapes = RedditCloneShapes.shapes,
         content = content
     )
 }
