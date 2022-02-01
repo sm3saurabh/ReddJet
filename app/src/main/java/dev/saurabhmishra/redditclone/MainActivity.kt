dc90e9946dd4830dc6e00989a3fb48218cbaaf82
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2022-02-01 15:56:48 +0530

    add: SignupScreen as the content for MainActivity

--- app/src/main/java/dev/saurabhmishra/redditclone/MainActivity.kt
+++ app/src/main/java/dev/saurabhmishra/redditclone/MainActivity.kt
@@ -10,32 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.saurabhmishra.redditclone.theme.RedditCloneTheme
+import dev.saurabhmishra.redditclone.ui.signup.SignupScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RedditCloneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                SignupScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RedditCloneTheme {
-        Greeting("Android")
     }
 }
