b4fad009b1fdb3eabd952aabc4b57e4d741cf30d
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2022-02-11 15:43:50 +0530

    fix: order of subreddits. fix: incorrect video assets name

--- app/src/main/java/dev/saurabhmishra/redditclone/provider/SignupSubRedditsProvider.kt
+++ app/src/main/java/dev/saurabhmishra/redditclone/provider/SignupSubRedditsProvider.kt
@@ -10,12 +10,12 @@ class SignupSubRedditsProviderImpl: SignupSubRedditsProvider {
 
   override fun getListOfSubReddits(): List<SubReddit> {
     return listOf(
-      SubReddit("Reddit", "https://cdn.videvo.net/videvo_files/video/free/2012-07/large_watermarked/FreeSmokeYoutube_preview.mp4"),
-      SubReddit("Anything", "https://cdn.videvo.net/videvo_files/video/free/2019-07/large_watermarked/190625_04_CityMisc_HD_05_preview.mp4"),
-      SubReddit("r/ContagiousLaughter", "https://player.vimeo.com/external/436376002.sd.mp4?s=158eaf12c8cbf5006eec7d43035231bb2e5a5778&profile_id=165&oauth2_token_id=57447761"),
-      SubReddit("r/Art", "https://player.vimeo.com/external/324597323.sd.mp4?s=1c76afc658a0f38cc1a464c9777103b9225127db&profile_id=164&oauth2_token_id=57447761"),
-      SubReddit("r/ShowerThoughts", "https://player.vimeo.com/external/483221534.sd.mp4?s=624a02586713da8f3117a5dafa1f58e3d3d35093&profile_id=165&oauth2_token_id=57447761"),
-      SubReddit("r/Gaming", "https://player.vimeo.com/external/500628042.sd.mp4?s=1aef9b2062713668cff84486e90ed511329cc31e&profile_id=165&oauth2_token_id=57447761"),
+      SubReddit("r/ContagiousLaughter", "asset:///contagious_laughter_video.mp4"),
+      SubReddit("r/Art", "asset:///art_video.mp4"),
+      SubReddit("r/ShowerThoughts", "asset:///shower_thoughts_video.mp4"),
+      SubReddit("r/Gaming", "asset:///gaming_video.mp4"),
+      SubReddit("Reddit", "asset:///reddit_video.mp4"),
+      SubReddit("Anything", "asset:///anything_video.mp4"),
     )
   }
 
