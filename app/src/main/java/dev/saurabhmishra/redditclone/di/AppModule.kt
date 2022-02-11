35e0c7db10e76f1287915bea7ba759dd5bb7f69b
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2022-02-11 15:36:14 +0530

    add: Exoplayer through dependency injection

--- app/src/main/java/dev/saurabhmishra/redditclone/di/AppModule.kt
+++ app/src/main/java/dev/saurabhmishra/redditclone/di/AppModule.kt
@@ -1,8 +1,11 @@
 package dev.saurabhmishra.redditclone.di
 
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dev.saurabhmishra.redditclone.provider.SignupSubRedditsProvider
 import dev.saurabhmishra.redditclone.provider.SignupSubRedditsProviderImpl
@@ -14,5 +17,10 @@ object AppModule {
   @Provides
   fun provideSignupSubRedditsProvider(): SignupSubRedditsProvider {
     return SignupSubRedditsProviderImpl()
+  }
+
+  @Provides
+  fun provideExoPlayer(@ApplicationContext context: Context): ExoPlayer {
+    return ExoPlayer.Builder(context).build()
   }
 }
